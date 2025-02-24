416c7ffbfb77fab4bfda612a4e4098b5a2a2418c
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-02-25 06:51:59 +0700

    ⚡️ Reusing Action via request Methods(Event Form with two methods (patch, post)
    Reusing Action via request Methods(Event Form with two methods (patch, post)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,9 @@ import EventsPage, {
   loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
   {
@@ -42,13 +43,14 @@ const router = createBrowserRouter([
               {
                 path: "edit",
                 element: <EditEventPage />,
+                action: manipulateEventAction,
               },
             ],
           },
           {
             path: "new",
             element: <NewEventPage />,
-            action: newEventAction,
+            action: manipulateEventAction,
           },
         ],
       },
